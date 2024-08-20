7ad98f8e7012765e99009f5a3a9950ac7b8df64d
lokeshgaddam20 <gaddamlokesh20@gmail.com>
2024-08-20 10:43:03 +0530

    Minor commit - Upload service✅

--- upload_service/routes/upload.route.js
+++ upload_service/routes/upload.route.js
@@ -1,14 +1,16 @@
-import express from "express"
-//import uploadFileToS3 from "../controllers/upload.controller.js";
-import { initializeUpload, uploadChunk, completeUpload } from
-"../controllers/multipartupload.controller.js";
+import express from "express";
+import { initializeUpload, uploadChunk, completeUpload, uploadToDb } from "../controllers/multipartupload.controller.js";
 import multer from 'multer';
 const upload = multer();
+
 const router = express.Router();
-// Route for initializing upload
+
 router.post('/initialize', upload.none(), initializeUpload);
-// Route for uploading individual chunks
+
 router.post('/', upload.single('chunk'), uploadChunk);
-// Route for completing the upload
+
 router.post('/complete', completeUpload);
+
+router.post('/uploadToDb', uploadToDb);
+
 export default router;
