a84d77cb87e6f758a7b51299bd10139d458d3423
lokeshgaddam20 <gaddamlokesh20@gmail.com>
2024-08-20 10:41:25 +0530

    Multipart chunks parallel

--- upload_service/routes/upload.route.js
+++ upload_service/routes/upload.route.js
@@ -1,13 +1,14 @@
-import express from "express";
+import express from "express"
+//import uploadFileToS3 from "../controllers/upload.controller.js";
+import { initializeUpload, uploadChunk, completeUpload } from
+"../controllers/multipartupload.controller.js";
 import multer from 'multer';
-import uploadFileToS3 from "../controllers/upload.controller.js";
-
-const router = express.Router();
 const upload = multer();
-
-router.post('/', uploadFileToS3);
-
+const router = express.Router();
+// Route for initializing upload
+router.post('/initialize', upload.none(), initializeUpload);
+// Route for uploading individual chunks
+router.post('/', upload.single('chunk'), uploadChunk);
+// Route for completing the upload
+router.post('/complete', completeUpload);
 export default router;
-
-
-
