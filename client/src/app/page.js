b4d8906b40c8fc1226d7aaa7412c725998ee4160
RishikaHub <rishika.bussa31@gmail.com>
2025-04-29 21:05:02 +0530

    Ft. Watch service init

--- client/src/app/page.js
+++ client/src/app/page.js
@@ -1,9 +1,11 @@
 import UploadForm from "./upload/page";
+import YouTubeHome from "./pages/youtubehome";
 
 export default function Home() {
   return (
     <div>
       <UploadForm/>
+      <YouTubeHome/>
     </div>
   );
 }
